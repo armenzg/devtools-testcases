633ea78c23c91482657cc62fa725de0fab63a4b3
Armen Zambrano G <armenzg@gmail.com>
2018-12-07 20:18:38 -0500

    Initial React app

---
+++ issue7445/src/App.jsx
@@ -0,0 +1,23 @@
+import { hot } from 'react-hot-loader';
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    name: 'issue7445',
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <div className="App">
+        <h1>
+          Welcome to
+          {name}
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
